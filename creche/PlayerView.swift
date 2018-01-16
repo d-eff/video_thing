b6dba94f393ffcce52e4f8b630da3a8f63a4e7d3
David Fernandez <dfernandez06@gmail.com>
2018-01-15 22:36:22 -0500

    Automute videos

--- creche/PlayerView.swift
+++ creche/PlayerView.swift
@@ -10,7 +10,7 @@ import WebKit
 
 class Player: WKWebView {
     //all props to https://stackoverflow.com/questions/34429042/autoplay-on-video-in-uiwebview-ios for the autoplay
-    let youTubeVideoHTML: String = "<!DOCTYPE html><html><head><style>body{margin:0px 0px 0px 0px;}</style></head> <body> <div id=\"player\"></div> <script> var tag = document.createElement('script'); tag.src = \"http://www.youtube.com/player_api\"; var firstScriptTag = document.getElementsByTagName('script')[0]; firstScriptTag.parentNode.insertBefore(tag, firstScriptTag); var player; function onYouTubePlayerAPIReady() { player = new YT.Player('player', { width:'%0.0f', height:'%0.0f', videoId:'%@', events: { 'onReady': onPlayerReady, } }); } function onPlayerReady(event) { event.target.playVideo(); } </script> </body> </html>"
+    let youTubeVideoHTML: String = "<!DOCTYPE html><html><head><style>body{margin:0px 0px 0px 0px;}</style></head> <body> <div id=\"player\"></div> <script> var tag = document.createElement('script'); tag.src = \"http://www.youtube.com/player_api\"; var firstScriptTag = document.getElementsByTagName('script')[0]; firstScriptTag.parentNode.insertBefore(tag, firstScriptTag); var player; function onYouTubePlayerAPIReady() { player = new YT.Player('player', { width:'%0.0f', height:'%0.0f', videoId:'%@', events: { 'onReady': onPlayerReady, } }); } function onPlayerReady(event) { event.target.mute(); event.target.playVideo(); } </script> </body> </html>"
     
     var html: String
     
